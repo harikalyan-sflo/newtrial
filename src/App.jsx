7ff906579fb605a22e8cee531342c13f9f954b6f
harikalyan-sflo <harikalyan.balaji@shopflo.com>
2025-01-03 18:27:47 +0530

    refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-
-
 function App() {
-
   return (
-   <div>
-    <h1>This is for sortment</h1>
-   </div>
-  )
+    <div>
+      <h1>This is for sortment</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
